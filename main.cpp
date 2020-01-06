9543329c70d24f7dc96ed0467e3b0cdaa7e98e1d
daisuke.oku <w.40141@gmail.com>
2020-01-06 13:32:49 +0900

    edit

--- main.cpp
+++ main.cpp
@@ -1,15 +1,29 @@
 #include <algorithm>
 #include <iostream>
 #include <iterator>
+#include <string.h>
 #include <vector>
+
+struct IntLike {
+    int data;
+    int value=10;
+
+    IntLike(int data, int value=1)
+        : data(data), value(value)
+    {}
+
+    IntLike operator +(IntLike const &other){
+        return  IntLike{data + other.data};
+}
+};
+
+
 int main(){
-    auto print = [](auto value){std::cout << value << "\n";};
-    std::vector<int> v = {1, 2, 3, 4, 5};
-    auto gen_zero = [](auto value){return &value;};
-    std::generate(std::begin(v), std::end(v), gen_zero);
-    std::for_each(std::begin(v), std::end(v), print);
-    // std::for_each(std::begin(v), std::end(v), print);
-    // std::vector<int> s = std::for_each(std::begin(v), std::end(v), gen_tra);
-    // std::for_each(std::begin(v), std::end(v), [](auto value){std::cout<< value << ", "s;});
-    // std::for_each(std::begin(v), std::end(v), [](auto &value){value = value + 3;});
+    IntLike x{1};
+    IntLike y{2, 4};
+    auto z = x + y;
+    std::cout << z.data << "\n"s;
+    std::cout << x.value << "\n"s;
+    std::cout << y.value << "\n"s;
+    std::cout << z.value;
 }
